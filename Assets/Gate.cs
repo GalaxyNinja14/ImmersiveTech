f62d45229a31675f65ca237cc201011f0f29fd85
Nathan Stockdale <74925317+GalaxyNinja14@users.noreply.github.com>
2024-04-29 22:59:40 +0100

    Secondary Interaction

---
+++ Assets/Gate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    void Start()
+    {
+        OpenGate();
+        GameEvents.current.eventCradleActive += OpenGate;
+        GameEvents.current.eventCradleDeactive += CloseGate;
+    }
+
+    private void OpenGate()
+    {
+        animator.SetBool("open", true);
+    }
+
+    private void CloseGate()
+    {
+        animator.SetBool("open", false);
+    }
+}
